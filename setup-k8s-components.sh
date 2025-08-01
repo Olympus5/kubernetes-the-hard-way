c0674ee5bc24d0be4aa86315f0d2f41fb9e90aa4
olympus5 <erwan.iquel@gmail.com>
2025-08-01 10:01:01 +0200

    cleanup jumpbox provisionning script

--- setup-k8s-components.sh
+++ setup-k8s-components.sh
@@ -4,28 +4,28 @@ apt-get update
 apt-get install -y wget curl vim openssl git
 
 if [ ! -d "/home/vagrant/kubernetes-the-hard-way/.git" ]; then
-    cd /home/vagrant
+    cd /home/vagrant || exit
     rm -rf kubernetes-the-hard-way
     git clone --depth 1 https://github.com/kelseyhightower/kubernetes-the-hard-way.git
 fi
 
-cd kubernetes-the-hard-way
+cd /home/vagrant/kubernetes-the-hard-way || exit
 
 arch=$(dpkg --print-architecture)
 
 if [ ! -d "/home/vagrant/kubernetes-the-hard-way/downloads" ]; then
-    wget -q --show-progress --https-only --timestamping -P downloads -i downloads-${arch}.txt
+    wget -q --show-progress --https-only --timestamping -P downloads -i "downloads-${arch}.txt"
 
     mkdir -p downloads/{clients,cni-plugins,controller,worker}
 
-    tar -xvf downloads/crictl-v1.32.0-linux-${arch}.tar.gz -C downloads/worker/
-    tar -xvf downloads/containerd-2.1.0-beta.0-linux-${arch}.tar.gz --strip-components=1 -C downloads/worker/
-    tar -xvf downloads/cni-plugins-linux-${arch}-v1.6.2.tgz -C downloads/cni-plugins/
-    tar -xvf downloads/etcd-v3.6.0-rc.3-linux-${arch}.tar.gz -C downloads/ --strip-components=1 etcd-v3.6.0-rc.3-linux-${arch}/etcdctl etcd-v3.6.0-rc.3-linux-${arch}/etcd
+    tar -xvf "downloads/crictl-v1.32.0-linux-${arch}.tar.gz" -C downloads/worker/
+    tar -xvf "downloads/containerd-2.1.0-beta.0-linux-${arch}.tar.gz" --strip-components=1 -C downloads/worker/
+    tar -xvf "downloads/cni-plugins-linux-${arch}-v1.6.2.tgz" -C downloads/cni-plugins/
+    tar -xvf "downloads/etcd-v3.6.0-rc.3-linux-${arch}.tar.gz" -C downloads/ --strip-components=1 "etcd-v3.6.0-rc.3-linux-${arch}/etcdctl" "etcd-v3.6.0-rc.3-linux-${arch}/etcd"
     mv downloads/{etcdctl,kubectl} downloads/clients/
     mv downloads/{etcd,kube-apiserver,kube-controller-manager,kube-scheduler} downloads/controller/
     mv downloads/{kubelet,kube-proxy} downloads/worker/
-    mv downloads/runc.${arch} downloads/worker/runc
+    mv "downloads/runc.${arch}" downloads/worker/runc
 
     rm -rf downloads/*gz
 
