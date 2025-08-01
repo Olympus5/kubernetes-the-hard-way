828d9ee54915194486e1a956260f7f8ddf78c65a
olympus5 <erwan.iquel@gmail.com>
2025-08-01 15:40:11 +0200

    enable ssh password auth on k8s cluster

--- enable-root-ssh-k8s-cluster.sh
+++ enable-root-ssh-k8s-cluster.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sed -i 's/^#[[:blank:]]*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/^PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl restart sshd
